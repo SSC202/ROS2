63c4f8f49c269e9947028750de27158ec8772282
SSC202 <e22750706642022@163.com>
2023-08-29 03:28:34 +0000

    ROS2 节点创建

--- ROS2_Test/Code/2_ROS2_Node_Build_ws/build/node_build/build/lib/node_build/node_build.py
+++
@@ -1,9 +0,0 @@
-import rclpy
-from rclpy.node import Node
-
-def main(args = None):
-    rclpy.init(args=args)
-    node = Node("node_build")
-    node.get_logger().info("Hello World")
-    rclpy.spin(node)
-    rclpy.shutdown()
--- ROS2_Test/Code/2_ROS2_Node_Build_ws/build/node_build/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/data/ROS2_Test/Code/2_ROS2_Node_Build_ws/install/node_build'

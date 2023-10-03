a96fe00e017571ff0cd67a54173e042ee87ea53c
SSC202 <e22750706642022@163.com>
2023-10-03 12:27:04 +0000

    Microros Agent 构建

---
+++ ROS2_Test/Code/A1_microros_ws/build/micro_ros_agent/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /data/ROS2/ROS2_Test/Code/A1_microros_ws/src/micro-ROS-Agent/micro_ros_agent
+# Build directory: /data/ROS2/ROS2_Test/Code/A1_microros_ws/build/micro_ros_agent
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

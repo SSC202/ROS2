69b5e51eab3b2fe34aac17757ceaeca05845b672
SSC202 <e22750706642022@163.com>
2023-10-07 13:29:06 +0000

    LD14乐动雷达驱动并使用Rviz可视化

--- ROS2_Test/Code/12_Sensor_ws/install/setup.ps1
+++ ROS2_Test/Code/12_Sensor_ws/install/setup.ps1
@@ -23,6 +23,7 @@ function _colcon_prefix_chain_powershell_source_script {
 
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/humble\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/data/ROS2/ROS2_Test/Code/10_URDF_model/install\local_setup.ps1"
 
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
--- ROS2_Test/Code/12_Sensor_ws/install/setup.sh
+++ ROS2_Test/Code/12_Sensor_ws/install/setup.sh
@@ -34,6 +34,10 @@ _colcon_prefix_chain_sh_source_script() {
 COLCON_CURRENT_PREFIX="/opt/ros/humble"
 _colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
 
+# setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
+COLCON_CURRENT_PREFIX="/data/ROS2/ROS2_Test/Code/10_URDF_model/install"
+_colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
+
 
 # source this prefix
 # setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
